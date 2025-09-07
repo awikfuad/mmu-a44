d7bf8aee5a2a34bb00240104ae0c28cfbaa22b2e
awikfuad <133140126+awikfuad@users.noreply.github.com>
2025-09-07 11:13:57 +0700

    new

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1383078769"
+    serviceWorkerVersion: "1383550885"
   }
 });
